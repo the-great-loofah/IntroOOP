6d3a1fa672d732f5e09aa0c9dcbed14df868c725
AngelBarron2 <angel.barroncastro@gmail.com>
2025-06-10 23:21:34 -0600

    Add files via upload

--- connect4/Program.cs
+++ connect4/Program.cs
@@ -2,37 +2,19 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace connect4
-{   public class Player
+{
+    public class Player
     {
-        public Player(string player1name)
+        public string Name { get; }
+
+        public Player(string name)
         {
-
-        }
-
-        public Player(string player1name, string player2name)
-        {
-
-        }
-
-        public void CreatePlayer(string number)
-        {
-            if (number == "1")
-            {
-
-            }
-            else if (number == "2")
-            {
-
-            }
-            else
-            {
-                Console.WriteLine("Invalid player count");
-                return;
-            }
+            Name = name;
         }
     }
 
@@ -41,20 +23,19 @@ namespace connect4
         public int Height;
         public int Width;
         public string[,] Array;
+
         public Board(int width, int height)
         {
             Width = width;
             Height = height;
-
             Array = new string[Width + 1, Height + 1];
-
         }
 
         public void CreateBoard()
-        {   
+        {
             for (int i = 0; i < Height; i++)
             {
-                for(int j = 0; j < Width; j++)
+                for (int j = 0; j < Width; j++)
                 {
                     Array[i, j] = "#";
                 }
@@ -63,7 +44,7 @@ namespace connect4
             for (int i = 0; i < Height; i++)
             {
                 Console.Write("|   ");
-                for(int j = 0; j < Width; j++)
+                for (int j = 0; j < Width; j++)
                 {
                     Console.Write(Array[i, j] + " ");
                 }
@@ -72,19 +53,34 @@ namespace connect4
             }
         }
 
-        public void Choice(int row, int column)
+        public void Choice(int column)
         {
 
         }
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             string i;
-            Board board = new Board(7, 6);
+            Board board = new Board(6, 7);
+            CreatePlayers();
             board.CreateBoard();
-            Console.WriteLine("How many players, 1 or 2?");
+            
+        }
+
+        static void CreatePlayers()
+        {
+            Console.Write("Player 1 Choose your name: ");
+            string player1Name = Console.ReadLine();
+            Player player1 = new Player(player1Name);
+
+            Console.Write("Player 2 Choose your name: ");
+            string player2Name = Console.ReadLine();
+            Player player2 = new Player(player2Name);
+            Console.Write($"your names are {player1.Name} and {player2.Name}\n");
+
         }
     }
 }
