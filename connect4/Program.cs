668b8be772a954f1ed42211eeafa712614474022
the-great-loofah <163227822+the-great-loofah@users.noreply.github.com>
2025-06-24 18:59:34 -0600

    Final cleanup
    Finished product... I think?

--- connect4/Program.cs
+++ connect4/Program.cs
@@ -2,89 +2,322 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace connect4
-{   public class Player
+namespace connect4 //Ignore commented out code please.
+{
+    //==========================Player Object==============================================
+    public abstract class Player //changed to abstract to allow for an AI split dreived from these base attributes.
     {
-        public Player(string player1name)
-        {
+        public string Name { get; set; }
+        public int Wins;
+        public char Token { get; set; } //assigned X or O in the game creation method.
 
+        public Player(string name)
+        {
+            Name = name;
+            Wins = 0;
+        }
+     
+
+    }
+    //================================Human player object============================================
+    public class HumanPlayer : Player //inherited from player, allows for a split to create AI from aswell.
+    {
+        public HumanPlayer(string name) : base(name)
+        {
+            Name = name;
+            Wins = 0;
         }
 
-        public Player(string player1name, string player2name)
+    }
+    //=================================AI Player Object===========================================
+    public class AiPlayer : Player
+    {
+        public AiPlayer(string name) : base(name)
         {
-
-        }
-
-        public void CreatePlayer(string number)
-        {
-            if (number == "1")
-            {
-
-            }
-            else if (number == "2")
-            {
-
-            }
-            else
-            {
-                Console.WriteLine("Invalid player count");
-                return;
-            }
+            Name = name;
+            Wins = 0;
         }
     }
-
+    //================================Board Object================================================
     public class Board
     {
-        public int Height;
-        public int Width;
-        public string[,] Array;
-        public Board(int width, int height)
+        private int Height = 6;
+        private int Width = 7;
+        public char[,] Array; 
+        public Board()
         {
-            Width = width;
-            Height = height;
-
-            Array = new string[Width + 1, Height + 1];
-
+            Array = new char[Height, Width];
         }
-
-        public void CreateBoard()
-        {   
-            for (int i = 0; i < Height; i++)
+        //===============================Board Creation method==========================================
+        public void CreateBoard()// was printing twice so changed this method
+        {
             {
-                for(int j = 0; j < Width; j++)
+                for (int i = 0; i < Array.GetLength(0); i++)
                 {
-                    Array[i, j] = "#";
+                    for (int j = 0; j < Array.GetLength(1); j++)
+                    {
+                        Array[i, j] = '#';
+                    }
                 }
             }
 
-            for (int i = 0; i < Height; i++)
+        }
+        //======Board print method=========================
+        public void PrintBoard() //This method reprints the board after every move. board.PrintBoard should be invoked after EVERYMOVE
+        {
+            for (int i = 0; i < Height; i++) //Prints the board with the current values in the array                                 //| DEBUG|
             {
-                Console.Write("|   ");
-                for(int j = 0; j < Width; j++)
+                Console.Write("|   "); //When the line starts, prints the board border
+                for (int j = 0; j < Width; j++)
                 {
-                    Console.Write(Array[i, j] + " ");
+                    Console.Write(Array[i, j] + " ");//Adds a space between each cell
                 }
-                Console.Write("   |");
+                Console.Write("   |"); //When the line ends, print the board border
                 Console.WriteLine(" ");
             }
+
+            Console.Write("|   ");
+            for (int i = 0; i < Width; i++)
+            {
+                Console.Write(i + " "); //Prints the column number underneath the board
+            }
+            Console.WriteLine("   |");
         }
 
-        public void Choice(int row, int column)
+        public bool DropToken(int column, char token)
         {
-
+            for (int row = Array.GetLength(0) - 1; row >= 0; row--)
+            {
+                if (Array[row, column] == '#')
+                {
+                    Array[row, column] = token; // places true if token is places
+                    return true;
+                }
+            }
+            return false; // returns false if collunm is full
         }
     }
-    internal class Program
+    //============================================Game Creation and Gameplay============================================================
+    public class CreateGame
     {
-        static void Main(string[] args)
+        private Board Arena;
+        private Player Player1;
+        private Player Player2;
+        private bool Win = false; //checks for a player winning
+        private bool GameEnd = false; // singular flag to break the while loop that runs the game.
+
+        public CreateGame(Board board, Player player1) // overloaded constructor for single player
         {
-            string i;
-            Board board = new Board(7, 6);
-            board.CreateBoard();
-            Console.WriteLine("How many players, 1 or 2?");
+            Arena = board;
+            Player1 = player1;
+            Player2 = new AiPlayer("AI"); // not sure how to write that AI mode not ready and then loop back to the gamemanager
+        }
+        public CreateGame(Board board, Player player1, Player player2)
+        {
+            Arena = board;
+            Player1 = player1;
+            Player2 = player2;
+        }
+
+        private void GameStart() //Still trying to figure out how we want to do this part. We could pass the board to a turn object to make the moves and then send it back to a method in here?
+        {
+            List<Player> playerList = new List<Player>();
+
+            playerList.Add(Player1);
+            playerList.Add(Player2);
+
+            playerList[0].Token = 'X';
+            playerList[1].Token = 'O';
+
+            Arena.CreateBoard();
+            Arena.PrintBoard();
+        }
+
+        private void GamePlay() //drops tokens
+        {
+            List<Player> players = new List<Player> { Player1, Player2 };
+            int currentPlayer = 0;//keeps track of wich player is playing
+            GameEnd = false; //resets the game end flag so the game can be played again.
+
+            while (!GameEnd) // will run until either a draw or win conditon(not yet implemented
+            {   
+                Player CurrentPlayer = players[currentPlayer];//checks whos turn it is
+                Console.WriteLine($"\n{CurrentPlayer.Name}'s Turn (Token: {CurrentPlayer.Token})");//tells whos turn it is and what there token is
+                Console.Write("Choose a column (0–6): ");
+
+                string input = Console.ReadLine(); //reads player choice
+                int column; //variable to hold collumn choice
+
+                if (!int.TryParse(input, out column) || column < 0 || column >= 7)// converts string to into integer. will fail if it can't be converted
+                {                                                                 //or it the integer is outside of the 0-6 range
+                    Console.WriteLine("Invalid column. Enter a number from 0 to 6.");
+                    continue;// redoes the loop and asks the same player again
+                }
+
+                bool success = Arena.DropToken(column, CurrentPlayer.Token);// drops token, will come back false if collumn is full
+                if (!success)
+                {
+                    Console.WriteLine("Column is full. Try another one.");
+                    continue;
+                }
+                //WIN CONDITIONS START HERE
+                //DIAGONAL DOWN AND TO THE RIGHT WIN LOGIC
+                for (int i = 0; i <= Arena.Array.GetLength(0) - 4; i++)//this part keeps both i + 3 and j + 3 in bounds
+                {                                                      
+                    for (int j = 0; j <= Arena.Array.GetLength(1) - 4; j++)
+                    {
+                        if (Arena.Array[i, j] == CurrentPlayer.Token &&
+                            Arena.Array[i + 1, j + 1] == CurrentPlayer.Token &&
+                            Arena.Array[i + 2, j + 2] == CurrentPlayer.Token &&
+                            Arena.Array[i + 3, j + 3] == CurrentPlayer.Token)
+                        {
+                            Console.WriteLine("\n" + CurrentPlayer.Name + " Wins");
+                            GameEnd = true;
+                        }
+                    }
+                }
+
+                // //DIAGONAL DOWN AND TO THE LEFT LOGIC
+                for (int i = 0; i <= Arena.Array.GetLength(0) - 4; i++) //makes sure  j -3 doesn't go bolow column 0 and i + 3 stays smaller or equal to 5
+                {
+                    for (int j = 3; j < Arena.Array.GetLength(1); j++)
+                    {
+                        if (Arena.Array[i, j] == CurrentPlayer.Token &&
+                            Arena.Array[i + 1, j - 1] == CurrentPlayer.Token &&
+                            Arena.Array[i + 2, j - 2] == CurrentPlayer.Token &&
+                            Arena.Array[i + 3, j - 3] == CurrentPlayer.Token)
+                        {
+                            Console.WriteLine("\n" + CurrentPlayer.Name + " Wins");
+                            GameEnd = true;
+                        }
+                    }
+                }
+
+                // UP AND DOWN WIN LOGIC
+                for (int i = 0; i <= Arena.Array.GetLength(0) - 4; i++)//makes sure i + 3 doesn't go above row index 5
+                {
+                    for (int j = 0; j < Arena.Array.GetLength(1); j++)
+                    {
+                        if (Arena.Array[i, j] == CurrentPlayer.Token &&
+                            Arena.Array[i + 1, j] == CurrentPlayer.Token &&
+                            Arena.Array[i + 2, j] == CurrentPlayer.Token &&
+                            Arena.Array[i + 3, j] == CurrentPlayer.Token)
+                        {
+                            Console.WriteLine("\n" + CurrentPlayer.Name + " Wins");
+                            GameEnd = true;
+                        }
+                    }
+                }
+
+                // LEFT AND RIGHT WIN LOGIC
+                for (int i = 0; i < Arena.Array.GetLength(0); i++)
+                {
+                    for (int j = 0; j <= Arena.Array.GetLength(1) - 4; j++)//make sure j + 3 doesn't go above column index 6
+                    {
+                        if (Arena.Array[i, j] == CurrentPlayer.Token &&
+                            Arena.Array[i, j + 1] == CurrentPlayer.Token &&
+                            Arena.Array[i, j + 2] == CurrentPlayer.Token &&
+                            Arena.Array[i, j + 3] == CurrentPlayer.Token)
+                        {
+                            Console.WriteLine("\n" + CurrentPlayer.Name + " Wins");
+                            GameEnd = true;
+                        }
+                    }
+                }
+
+
+                //TIE GAME LOGIC
+                int counter = 0;
+                for(int i = 0; i < Arena.Array.GetLength(0); i++)
+                {
+                    for(int j = 0; j < Arena.Array.GetLength(1); j++)
+                    {
+                        if (Arena.Array[i,j] != '#')
+                        {
+                            counter++;
+                        }                      
+                    }
+                }
+                if(counter >= 42 && GameEnd != true) //tried a few different methods that didnt work. Ended up with this. Will only trigger tie game if a win condition hasnt occured
+                {
+                    GameEnd = true;
+                    Console.WriteLine("\nThe game was a tie");
+                }
+                counter = 0;//resets the counter after every pass through the array, redundancy incase future code needs to be placed here
+
+                Arena.PrintBoard();//reprints the board
+                
+                currentPlayer = 1 - currentPlayer;//switches turn, THIS IS A GENIUS PIECE OF CODE - J
+            }            
+        }
+        //====================================GAME START LOGIC===============================
+        public class GameManager//main logic goes here
+        {
+            public void Run()
+            {
+                Console.WriteLine("Welcome to Connect 4\nPlease input 2 for 2 player mode, or 'exit' to quit:\n(AI mode not implemented yet)");
+
+                string command = Console.ReadLine();
+
+                while (!command.Equals("exit", StringComparison.OrdinalIgnoreCase))// the EXIT and exit was giving soms problems, this way it ignores caps
+                {
+                    if(command == "1")
+                    {
+                        OnePlayerGame();
+                    }
+                    if (command == "2")
+                    {
+                        TwoPlayerGame();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input! Please type '2' to play or 'exit' to quit.");
+                    }
+
+                    Console.WriteLine("\nType '2' to play again or 'exit' to quit:");
+                    command = Console.ReadLine();
+                }
+
+                Console.WriteLine("\nThanks for playing!");
+            }
+
+            private void OnePlayerGame()
+            {
+                Console.WriteLine("Sorry, AI player has not yet been implemented");
+                Run();//repeats the game start question.
+            }
+
+            private void TwoPlayerGame()
+            {
+                Console.WriteLine("Please choose Player 1's name:");
+                string player1 = Console.ReadLine();
+                Player p1 = new HumanPlayer(player1); //Setting it up this way allows us to have a list composed of players or a player and AI 
+
+                Console.WriteLine("Please choose Player 2's name:");
+                string player2 = Console.ReadLine();
+                Player p2 = new HumanPlayer(player2); 
+
+                Board board = new Board();
+                CreateGame game = new CreateGame(board, p1, p2);
+
+                game.GameStart();
+                game.GamePlay(); // 
+            }
+        }
+        //=================================================================MAIN==========================================================================
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                GameManager manager = new GameManager();// moved logic into class GameManager
+                manager.Run();
+            }
+
         }
     }
 }
